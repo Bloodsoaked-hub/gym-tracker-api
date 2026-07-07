f66364722e0d8ba3f49004909e8279f31a76783a
Przemek Kwak <przemek.kwak153@gmail.com>
2026-07-07 21:08:59 +0200

    refactor: change router file structure

--- app/api/router.py
+++ app/api/router.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
-from app.api.routes.auth import router as auth_router
-from app.api.routes.users import router as users_router
-from app.api.routes.exercises import router as exercises_router
-from app.api.routes.workouts import router as workouts_router
-from app.api.routes.workout_exercises import router as workout_exercises_router
+
+from app.api.routes import auth, users, exercises, workouts, workout_exercises
 
 api_router = APIRouter()
 
-api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
-api_router.include_router(users_router, prefix="/users", tags=["users"])
-api_router.include_router(exercises_router, prefix="/exercises", tags=["exercises"])
-api_router.include_router(workouts_router, prefix="/workouts", tags=["workouts"])
-api_router.include_router(workout_exercises_router, prefix="/workouts/{workout_id}/exercises", tags=["workout_exercises"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(exercises.router, prefix="/exercises", tags=["exercises"])
+api_router.include_router(workouts.router, prefix="/workouts", tags=["workouts"])
+api_router.include_router(workout_exercises.router, prefix="/workouts/{workout_id}/exercises", tags=["workout_exercises"])
